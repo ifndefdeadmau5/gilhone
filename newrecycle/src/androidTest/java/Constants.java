1bac35a77a478d892740a6c1c3331200161646e3
SeongMin <ifndefdeadmau5@gmail.com>
2015-01-19 20:54:39 +0900

    1월19일 월요일 밤 standardtech 는 동작

---
+++ newrecycle/src/androidTest/java/Constants.java
@@ -0,0 +1,7 @@
+public interface Constants {
+
+    String KEY_ID = "keyId";
+
+    String NAME_INNER_CONTAINER = "innerContainer";
+
+}
---
+++ newrecycle/src/androidTest/java/d.java
@@ -0,0 +1,5 @@
+/**
+ * Created by SeongMin on 2015-01-19.
+ */
+public class d {
+}
